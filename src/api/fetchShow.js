9c9912ccd796d5fb07809654add9dba973e4fb25
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2021-02-22 22:20:15 -0500

    async

---
+++ src/api/fetchShow.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+
+export const fetchShow = () => {
+    return axios
+    .get(
+      "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    )
+    .then(res => {
+        console.log(res)
+      return(res);
+    })
+    .catch(err => {
+        console.error("error fetching data from api, err: ", err.message);
+        return (err);
+      });
+}; // or res.data, however you want to set that up
